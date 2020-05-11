c6f2a7270b7c11384be473a80ccff8df61b2b195
JIBaker92 <57137154+JIBaker92@users.noreply.github.com>
2020-05-11 16:27:43 +0100

    added all current needed fillables

--- app/Character.php
+++ app/Character.php
@@ -6,5 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Character extends Model
 {
-    //
+    protected $fillable = [
+        'title', 'description', 'classification_name', 'faction_name'
+    ];
 }
--- app/Faction.php
+++ app/Faction.php
@@ -6,5 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Faction extends Model
 {
-    //
+    protected $fillable = [
+        'faction_name'
+    ];
 }
